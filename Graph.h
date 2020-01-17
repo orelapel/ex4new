f337bc9afde73ade301eaa04ab27ad95d2980838
orelapel <orellicht@gmail.com>
2020-01-17 05:09:50 -0800

    graph

---
+++ Graph.h
@@ -0,0 +1,33 @@
+//
+// Created by orelapel on 1/17/20.
+//
+
+#ifndef EX4_GRAPH_H
+#define EX4_GRAPH_H
+
+
+#include<iostream>
+#include <list>
+
+using namespace std;
+
+// This class represents a directed graph using
+// adjacency list representation
+class Graph {
+    int V;    // No. of vertices
+
+    // Pointer to an array containing adjacency
+    // lists
+    list<int> *adj;
+public:
+    Graph(int V);  // Constructor
+
+    // function to add an edge to graph
+    void addEdge(int v, int w);
+
+    // prints BFS traversal from a given source s
+    void BFS(int s);
+};
+
+
+#endif //EX4_GRAPH_H
